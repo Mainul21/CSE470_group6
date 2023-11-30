d1cc5ce843003c31a9cda6a6b2e8972a21b48849
Mainul Hossain <138763613+Mainul21@users.noreply.github.com>
2023-11-30 23:44:40 +0600

    sprint 2

---
+++ storage/framework/views/97ba9a23537e44fa9884d1c66cb6cd81.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>food</title>
+    <link rel="stylesheet" href="<?php echo e(asset('css/BuyFoodStyles.css')); ?>">
+</head>
+<body>
+    <div class="menu">
+        <div class="heading">
+            <h1>Order Your Food</h1>
+        </div>
+        <div class="foodItems">
+            <img src="/food_images/burger.jpg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Smoky Hamburger</h5>
+                    <h5 class="price"> Tk500 </h5>
+                </div>
+                <p>Savor the rich and smoky flavors of our signature hamburger, 
+                    where perfectly grilled beef meets a tantalizing smoky twist 
+                    for an unforgettable taste experience.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="foodItems">
+            <img src="/food_images/chicken.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Honey Garlic Chicken</h5>
+                    <h5 class="price"> Tk300 </h5>
+                </div>
+                <p>Indulge in the perfect blend of sweet and savory with our Honey Garlic Chicken, 
+                    featuring succulent chicken glazed in a luscious honey-infused garlic 
+                    sauce for a delectable culinary delight.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="foodItems">
+            <img src="/food_images/soup.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Manchow Soup</h5>
+                    <h5 class="price"> Tk450 </h5>
+                </div>
+                <p>Experience the bold and aromatic allure of Manchow Soup, a harmonious blend of spicy, 
+                    tangy, and flavorful broth enriched with vegetables, noodles, and aromatic 
+                    spices for a truly heartwarming bowl.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="foodItems">
+            <img src="/food_images/Spaghetti.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Basil Pesto Spaghetti</h5>
+                    <h5 class="price"> Tk150 </h5>
+                </div>
+                <p>Savor the essence of Italy with our Basil Pesto Spaghetti, where al dente pasta is generously 
+                    coated in a vibrant basil pesto sauce, creating a symphony of fresh flavors in every forkful.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="foodItems">
+            <img src="/food_images/salmon.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Roast Salmon</h5>
+                    <h5 class="price"> Tk180 </h5>
+                </div>
+                <p>Delight in the exquisite simplicity of Roast Salmon, a succulent dish expertly seasoned and 
+                    oven-roasted to perfection, offering a melt-in-your-mouth experience with every flavorful bite.</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+
+        <div class="foodItems">
+            <img src="/food_images/Sandwich.jpeg">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>BBQ Sandwich</h5>
+                    <h5 class="price"> Tk280 </h5>
+                </div>
+                <p>Enjoy a taste explosion with our BBQ Sandwich tender, smoky barbecue-infused meat nestled 
+                    between soft buns, creating a mouthwatering symphony of savory flavors in every bite.F</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html><?php /**PATH D:\BRACU\Current Semester\CSE470\Project\ShareAndCare\resources\views/BuyFoodPage.blade.php ENDPATH**/ ?>
